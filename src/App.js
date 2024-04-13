bbc97a745889366fe37d6292dba59e5e48a1d789
saran@linux <saranmass685@gmail.com>
2024-04-13 18:54:39 +0530

    practice reactjs

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>Hey this is My World</p>
+                <a className="App-link" href="https://saran.selfmade.one" target="_blank" rel="noopener noreferrer">Saran</a>
+            </header>
+        </div>
+    );
+}
+
+export default App;
